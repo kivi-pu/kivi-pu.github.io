c63c33c93cccea39a5ad5dd90884bfe041a24ec7
Just Me <just806me@gmail.com>
2020-11-14 17:20:25 +0200

    added products list

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import 'fomantic-ui-css/semantic.min.css'
+
+import ProductsList from './products-list'
 
 ReactDOM.render(
   <React.StrictMode>
+    <ProductsList />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
